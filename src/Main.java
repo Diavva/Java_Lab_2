25e0811255e9941220728aa461f3cf417465b7c4
Diavva <Karmasin@interia.pl>
2022-01-10 14:52:18 +0100

    Points 2 and 3. added index number

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
         /* cw.1 pętle while */
 
-        /* zad 1 - Napisać za pomocą pętli do-while kod który będzie iterował od liczby 1000 w dół do 0.
+        /* + zad 1 - Napisać za pomocą pętli do-while kod który będzie iterował od liczby 1000 w dół do 0.
           Jeśli liczba będzie podzielna przez 2 ostatnie cyfry waszego indexu (podać nr indexu)
           to dać komunikat "liczba XX podzielna przez YY" (XX-liczba podzielna YY-podzielnik)
         */
@@ -23,7 +23,9 @@ public class Main {
         */
 
 
-        /* zad 1 */
+        /* zad 1
+         * numer indeksu/albumu 62489
+         * */
 
         int i = 1000;
         while (i > 0) {
@@ -38,6 +40,56 @@ public class Main {
                     }
 
 
+                    /* zad 2 abc
+                     */
+
+                    int[] tab = {5, 10, 15, 20, 25};
+
+                    int x = 0;
+
+                    while (x < tab.length) ;
+                    x++;
+
+                    System.out.println(tab[x] + " ");
+
+
+
+                    String[] stab = new String[]{"To", "jest", "nowa", "testowa", "tabela"};
+                    for (int j = 0; j < stab.length; j++) {
+                        System.out.print(stab[j] + " ");
+
+
+                        double[] dtab = {4.21, 3.5, 1.4, 2.09, 4.87};
+
+                        int z = 0;
+
+                        while (z < dtab.length) ;
+                        z++;
+
+                        System.out.println(dtab[z] + " ");
+
+                    /*
+                    zad 3
+                     */
+
+                        int tor = 0;
+
+                        while (true) {
+
+                            System.out.println("Numer: " + tor);
+                            tor ++;
+
+                            if (false)
+                            break;
+
+                        }
+
+
+
+
+
+
+                    }
                 }
             }
         }
