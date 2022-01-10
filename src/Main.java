e81ffddd2030ac5a8b729525d564a9535e3b5270
Diavva <Karmasin@interia.pl>
2022-01-10 18:28:16 +0100

    Point two done

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
@@ -28,6 +29,39 @@ public class Main {
             if (r % 9 == 0){
                 System.out.print( r + " jest podzielne przez 9 \n");
             }
+
+
+            /*
+            zad 2
+             */
+
+            Scanner scan = new Scanner(System.in);
+
+            System.out.println("Podaj liczbę nr 1: ");
+            int tab1 = scan.nextInt();
+            System.out.println(tab1 + " Podaj liczbę nr 2: ");
+            int tab2 = scan.nextInt();
+            System.out.println(tab1 + ", " + tab2 + " Podaj liczbę nr 3: ");
+            int tab3 = scan.nextInt();
+            System.out.println(tab1 + ", " + tab2 + ", " + tab3 + " Podaj liczbę nr 4: ");
+            int tab4 = scan.nextInt();
+            System.out.println(tab1 + ", " + tab2 + ", " + tab3 + ", " + tab4 + " Podaj liczbę nr 5: ");
+            int tab5 = scan.nextInt();
+
+            int tab11 = tab1 * 11;
+            int tab22 = tab2 * 11;
+            int tab33 = tab3 * 11;
+            int tab44 = tab4 * 11;
+            int tab55 = tab5 * 11;
+
+
+            int [] tab = {tab11, tab22, tab33, tab44, tab55};
+
+            for (int x : tab)
+            {
+                System.out.print(x + ", ");
+            }
+
         }
     }
 }}
